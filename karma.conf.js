ea6972c0d9bfbdce4edee7e07d3a3f5f6a4ddb41
Jcruz2002 <jesusdaviddelacruzarroyo@yahoo.com>
2026-06-04 20:36:37 -0500

    fix: karma.conf.js con frameworks+plugins completos para @angular/build:karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,40 @@
-module.exports = function (config) {
-  const cr = config.coverageReporter || {};
+const path = require('path');
 
-  // Extiende los reporters sin reemplazar el resto de la config de karma
-  config.coverageReporter = {
-    dir: cr.dir || require('path').join(__dirname, 'coverage'),
-    reporters: [
-      ...(cr.reporters || [{ type: 'html' }, { type: 'text-summary' }]),
-      { type: 'lcovonly',  subdir: '.', file: 'lcov.info'       },
-      { type: 'cobertura', subdir: '.', file: 'cobertura.xml'   }
-    ]
-  };
+// When Angular's @angular/build:karma sees a karmaConfig option, it does NOT inject
+// its built-in frameworks/plugins (getBaseKarmaOptions returns {} instead of
+// getBuiltInKarmaConfig). We must replicate that built-in config here and add
+// the extra coverage reporters we need for Azure DevOps.
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+    ],
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    coverageReporter: {
+      dir: path.join(__dirname, 'coverage', 'frontend-contratos'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly',  file: 'lcov.info'      },
+        { type: 'cobertura', file: 'cobertura.xml'  },
+      ],
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+      },
+    },
+    restartOnFileChange: true,
+  });
 };
